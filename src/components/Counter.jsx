ab3622fe9a20d7a7c10f1e7ea3add0f9b71a58fb
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-01 14:28:07 +0400

    setCount, count -> tovsiye olunan ve duzgun kod!

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,59 @@
+import { useState } from "react"
+
+// function Counter() {
+//     let count = 0
+//     // men sene funksiya verirem. Hemin funksiya ile deyis ki, mende render edim!
+//     return (
+//         <div>
+//             <h1>{count}</h1>
+//             <button
+//                 onClick={() => {
+//                     count++
+//                 }}
+//             >+</button>
+//             <button
+//                 onClick={() => {
+//                     count--
+//                 }}
+//             >-</button>
+//             <button
+//                 onClick={() => {
+//                     console.log(count)
+//                 }}
+//             >console</button>
+
+
+//         </div>
+//     )
+
+// }
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+
 function Counter() {
-    let count = 0
+    const [count, setCount] = useState(0) // use*->Hook (use-ile baslayan hook-lar adlanir)
+    //name-ingin adlandirilmasi [count ve setCount] olur
+
     // men sene funksiya verirem. Hemin funksiya ile deyis ki, mende render edim!
     return (
         <div>
             <h1>{count}</h1>
             <button
                 onClick={() => {
-                    count++
+                    setCount(count + 1)
                 }}
             >+</button>
             <button
                 onClick={() => {
-                    count--
+                    setCount(count - 1)
                 }}
             >-</button>
+            <button
+                onClick={() => {
+                    setCount(0)
+                }}
+            >0</button>
             <button
                 onClick={() => {
                     console.log(count)
